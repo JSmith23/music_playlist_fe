d61d0ed6b89ad93b1c306fef10c22b7046b6d6ab
jpclark6 <jpclark6@gmail.com>
2019-03-19 14:01:07 -0600

    Adds sass functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import './style.scss';
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
